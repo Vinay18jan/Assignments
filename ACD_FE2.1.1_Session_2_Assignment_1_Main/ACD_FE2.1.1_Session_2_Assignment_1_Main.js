fa65f54f22bb255cc8e73e16374d9b7c645c15cc
vinay kumar - AFS <vinay.kumar@artefactsys.com>
2018-02-05 15:10:19 +0530

    Assignment 2

---
+++ ACD_FE2.1.1_Session_2_Assignment_1_Main/ACD_FE2.1.1_Session_2_Assignment_1_Main.js
@@ -0,0 +1,4 @@
+var result = function (num1, num2) {
+    return "The result of adding " + num1 + " and " + num2 + " will be : " + (num1 + num2);
+};
+console.log(result(3, 2));
---
+++ ACD_FE2.1.1_Session_2_Assignment_1_Main/ACD_FE2.1.1_Session_2_Assignment_1_Main.ts
@@ -0,0 +1,5 @@
+var result = (num1, num2) => {
+    return "The result of adding "+num1+" and "+num2 +" will be : " +(num1 + num2);
+};
+
+console.log(result(3, 2));
